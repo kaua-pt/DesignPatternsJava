bba77d160baf551051a4751fb99528465d723179
Kauã Vinícius <kauaponte@hotmail.com>
2023-10-31 17:04:01 -0300

    cosoro

---
+++ Estruturais/Flyweigth/src/DeliveryLoc.java
@@ -0,0 +1,14 @@
+public class DeliveryLoc implements DeliveryFly {
+    private String estadoIntriseco;
+
+    public DeliveryLoc(String estadoIntriseco) {
+
+    }
+
+    @Override
+    public void entrega(String cliente, String entregador) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'entrega'");
+    }
+
+}
